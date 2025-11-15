d5f5b1bf3acbc287eb84dfc21482d5a38b7a6018
SHIVAM CHAUHAN <xploreshivam@gmail.com>
2025-11-15 16:49:26 +0530

    Create GoogleDriveIcon.tsx

---
+++ components/icons/GoogleDriveIcon.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const GoogleDriveIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={className}>
+        <path d="M19.32 4.25l-5.87 10.12-5.87-10.12h11.74zm-13.87.25l5.87 10.12-2.94 5.13-5.87-10.12h2.94zm10.94 15.25l-2.94-5.12 5.87-10.13h2.94l-5.87 15.25z"></path>
+    </svg>
+);
