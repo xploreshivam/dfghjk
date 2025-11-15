2384b61ca834c496f2d501d50db9222a4adc3498
SHIVAM CHAUHAN <xploreshivam@gmail.com>
2025-11-15 09:25:08 +0530

    Create types.ts

---
+++ services/types.ts
@@ -0,0 +1,29 @@
+
+export enum LogStatus {
+  INFO = 'info',
+  PENDING = 'pending',
+  SUCCESS = 'success',
+  ERROR = 'error',
+}
+
+export interface Log {
+  id: number;
+  message: string;
+  status: LogStatus;
+}
+
+export enum AssetType {
+  TITLE_LIST = 'title_list',
+  SCRIPT = 'script',
+  AUDIO = 'audio',
+  IMAGE = 'image',
+  THUMBNAIL = 'thumbnail',
+}
+
+export interface GeneratedAsset {
+  id: string;
+  type: AssetType;
+  name: string;
+  url: string;
+  blob: Blob;
+}
