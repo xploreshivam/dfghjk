23592fadfb5971133dd4a5877d9d4af88925ecaa
SHIVAM CHAUHAN <xploreshivam@gmail.com>
2025-11-15 11:13:26 +0530

    Update GoogleAuth.tsx

--- components/GoogleAuth.tsx
+++ components/GoogleAuth.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoogleDriveIcon } from './icons/GoogleDriveIcon.tsx';
+import { GoogleDriveIcon } from './icons/GoogleDriveIcon';
 
 interface GoogleAuthProps {
   isReady: boolean;
